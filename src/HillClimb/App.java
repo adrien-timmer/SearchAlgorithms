201ec16e86b805d145c94de1bdb4a093f6006220
Adrien Timmer <cbrtimmer@gmail.com>
2017-10-03 20:46:29 -0600

    Hill Climbing

---
+++ src/HillClimb/App.java
@@ -0,0 +1,8 @@
+package HillClimb;
+
+public class App {
+    public static void main(String[] args) {
+        HillClimbing hillClimbing = new HillClimbing();
+        hillClimbing.hillClimbing();
+    }
+}
